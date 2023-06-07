156c1bba6afba518dbe1df14848fd7b4c6487b47
MuhammedFurkanKaya <frknkybm@gmail.com>
2023-06-07 23:32:53 +0300

    End of the project

---
+++ TuglaKirma/Assets/Scripts/Puan.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puan : MonoBehaviour
+{
+    private int puan;
+    public void PuanArttir()
+    {
+        puan++;
+
+    }
+
+    public int PuaniAl()
+    {
+        return puan;
+    }
+
+    public void PuanSifirla()
+    {
+        puan = 0;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
